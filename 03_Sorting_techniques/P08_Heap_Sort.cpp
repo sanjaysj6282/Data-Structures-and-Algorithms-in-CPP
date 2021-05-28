54acfcd77fa5728cee58f52474ea5b686a141220
sanjay <sanjaysj6282@gmail.com>
2021-05-28 19:46:46 +0530

    Updated Heap sort

--- 03_Sorting_techniques/P08_Heap_Sort.cpp
+++ 03_Sorting_techniques/P08_Heap_Sort.cpp
@@ -2,6 +2,37 @@
 #include <vector>
 using namespace std;
 
+// One of best Sort to learn
+//With this u will get a clear understanding of heaps
+
+/*
+
+What is Binary Heap? 
+Complete BT is a BT in which every level, except possibly the last, is completely filled, 
+and all nodes are as far left as possible 
+(OR)  In array representation there should not be any space in b/w elements 
+Complete BT where items are stored in a special order such that the value in a 
+parent node is greater(or smaller) than the values in its two children nodes.
+The heap can be represented by a binary tree or array.
+
+If the parent node is stored at index I, the 
+Left child can be calculated by 2 * I + 1 
+Right child by 2 * I + 2 (assuming the indexing starts at 0).
+
+Heap Sort Algorithm for sorting in increasing order: 
+1. Build a max heap from the input data. 
+2. At this point, the largest item is stored at the root of the heap. 
+   Replace it with the last item of the heap followed by reducing the size of heap by 1. 
+   Finally, heapify the root of the tree. 
+3. Repeat step 2 while the size of the heap is greater than 1.
+
+How to build the heap? 
+Heapify procedure can be applied to a node only if its children nodes are heapified. 
+So the heapification must be performed in the bottom-up order.
+
+*/
+
+// Heapify down
 // To heapify a subtree rooted with node i which is
 // an index in arr[]. n is size of heap
 void heapify(vector<int> &a, int n, int i){
@@ -29,17 +60,17 @@ void heapify(vector<int> &a, int n, int i){
 void heapsort(vector<int> &a){
     int n =a.size();
     
-    // Build heap (rearrange array)
-    // from last non leaf element
+    // Step 1
+    // Build heap (rearrange array) from last non leaf element
     for(int i=n/2-1; i>= 0; i--)
         heapify(a, n, i);
     
+    // Step 2 and 3
     // One by one extract an element from heap
     for(int i=n-1; i>0; i--){
         // Move current root to end
         swap(a[0], a[i]);
-        // call max heapify on the reduced heap
-        // heapify upto i
+        // call max heapify on the reduced heap heapify upto i
         heapify(a, i, 0);
     }
 }
