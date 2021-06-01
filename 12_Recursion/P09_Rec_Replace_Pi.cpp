a0daa4abb19c4b5f2fa44f57ea3a13e79b34c1f2
sanjay <sanjaysj6282@gmail.com>
2021-06-01 18:29:46 +0530

    updated replace pi recursion

--- 12_Recursion/P09_Rec_Replace_Pi.cpp
+++ 12_Recursion/P09_Rec_Replace_Pi.cpp
@@ -8,13 +8,15 @@ void replacePi(string s){
     if(s.length() == 0)
         return;
 
-    if(s[0] == 'p' && s[1] == 'i')
+    if(s[0] == 'p' && s[1] == 'i'){
         cout << "3.14";
         replacePi(s.substr(2));
+    }
     
-    else
+    else{
         cout << s[0];
         replacePi(s.substr(1));
+    }
 }
 
 
