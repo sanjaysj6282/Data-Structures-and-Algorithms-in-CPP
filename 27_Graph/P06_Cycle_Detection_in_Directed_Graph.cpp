73403f688dfb160d176dd5fbaffcf63a53b94c8f
sanjay <sanjaysj6282@gmail.com>
2021-06-16 23:55:53 +0530

    Added Graph-Cycle-det-Dir

---
+++ 27_Graph/P06_Cycle_Detection_in_Directed_Graph.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <algorithm> 
+using namespace std;
+
+#define vi vector<int> 
+#define pi pair<int, int>
+#define ff first
+#define ss second
+#define vii vector<pi>
+#define vvi vector<vi> 
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+#define ll long long
+
+const int N=1e4+3, MOD=1e9+7;
+
+
+// DFS 
+bool isCycle(int src, vvi &adj, vector<bool> &vis, vi stack){  
+    stack[src]=true;
+    if(!vis[src]){
+        vis[src]=true;
+        for(auto i : adj[src]){
+            if(!vis[i] && isCycle(i, adj, vis, stack))
+                return true;
+
+            if(stack[i])
+                return true;
+        }
+    }
+    stack[src]=false;
+    return false;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vvi adj(n);
+
+    rep(i, 0, m){
+        int x, y;
+        cin >> x >> y;
+
+        adj[x].push_back(y);
+    }
+
+    bool cycle =false;
+    vi stack(n, 0);
+    vector<bool> vis(n, false);
+    rep(i, 0, n)
+        if(!vis[i] && isCycle(i, adj, vis, stack))
+            cycle = true;
+    
+    if(cycle)
+        cout << "Cycle is present" << endl;
+    else
+        cout << "Cycle is not present" << endl; 
+
+    return 0;
+}
