f47d68820ff01a2272528ce8eb7b39bc4e60d46a
sanjay <sanjaysj6282@gmail.com>
2021-06-16 23:38:49 +0530

    Added Graph-Cycle-det-Undir

---
+++ 27_Graph/P05_Cycle_Detection_in_Undirected_Graph.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <algorithm> 
+using namespace std;
+
+#define vi vector<int> 
+#define pi pair<int, int>
+#define ff first
+#define ss second
+#define vii vector<pi>
+#define vvi vector<vi> 
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+#define ll long long
+
+const int N=1e4+3, MOD=1e9+7;
+
+
+// DFS 
+bool isCycle(int src, vvi &adj, vector<bool> &vis, int parent){
+    vis[src]=true;
+    for(auto i: adj[src]){
+        if(i != parent){
+            if(vis[i])
+                return true;
+
+            if(!vis[i] && isCycle(i, adj, vis, src))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vvi adj(n);
+
+    rep(i, 0, m){
+        int x, y;
+        cin >> x >> y;
+
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    bool cycle =false;
+    vector<bool> vis(n, false);
+    rep(i, 0, n)
+        if(!vis[i] && isCycle(i, adj, vis, -1))
+            cycle = true;
+    
+    if(cycle)
+        cout << "Cycle is present" << endl;
+    else
+        cout << "Cycle is not present" << endl; 
+
+    return 0;
+}
