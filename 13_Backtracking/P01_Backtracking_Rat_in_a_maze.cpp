65393c6b59b7072f30df78962cf2f7de22b444db
sanjay <sanjaysj6282@gmail.com>
2021-06-20 19:50:46 +0530

    Updated graph

--- 13_Backtracking/P01_Backtracking_Rat_in_a_maze.cpp
+++ 13_Backtracking/P01_Backtracking_Rat_in_a_maze.cpp
@@ -21,6 +21,7 @@ bool isSafe(int **arr,int x,int y,int n){
 
     if(x<n && y<n && arr[x][y]==1)
         return true;
+
     return false;    
 }
 
@@ -33,16 +34,16 @@ bool ratinMaze(int **arr,int x,int y,int n,int **solArr){
 
     if(isSafe(arr,x,y,n)){
         solArr[x][y] = 1;
-        if(ratinMaze(arr,x+1,y,n,solArr)){
+        if(ratinMaze(arr,x+1,y,n,solArr))
+            return true; 
+        if(ratinMaze(arr,x,y+1,n,solArr))
             return true;
-        }
-        if(ratinMaze(arr,x,y+1,n,solArr)){
-            return true;
-        }
+        
         // VIMP
         solArr[x][y] = 0;      //backtracking
         return false;
     }
+
     return false;
 }
 
@@ -60,26 +61,23 @@ int main(){
     for(int i = 0; i < n; i++)
         arr[i] = new int[n];
         
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n;j++){
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n;j++)
             cin >> arr[i][j];
-        }
-    }
+
 
     int** solArr = new int*[n];
     for(int i = 0; i < n; i++){
         solArr[i] = new int[n];
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < n; j++)
             solArr[i][j] = 0;
-        }
     }
 
     cout << endl;
     if(ratinMaze(arr,0,0,n,solArr)){
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++)
                 cout << solArr[i][j] << " ";
-            }
             cout << endl;
         }
     }
