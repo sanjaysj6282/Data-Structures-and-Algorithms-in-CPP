033a14e611741e73d428316c7349ce354843507e
sanjay <sanjaysj6282@gmail.com>
2021-07-11 22:54:39 +0530

    updated queue

--- 18_Queue/P01_Queue.cpp
+++ 18_Queue/P01_Queue.cpp
@@ -9,47 +9,51 @@ class queue{
     int back;
 
     public:
-    queue(){
-        arr=new int[n];
-        front=-1;
-        back=-1;
-    }
-
-    void push(int x){
-        if(back==n-1){
-            cout << "Queue overflow" << endl;
-            return;
+        queue(){
+            arr=new int[n];
+            front=-1;
+            back=-1;
         }
-        back++;
-        arr[back]=x;
 
-        if(front==-1)
+        void push(int x){
+            if(back==n-1){
+                cout << "Queue overflow" << endl;
+                return;
+            }
+
+            back++;
+            arr[back]=x;
+
+            if(front==-1)
+                front++;
+        }
+
+        void pop(){
+            if((front==-1) || front>back){
+                cout << "No elements in queue" << endl;
+                return; 
+            }
+
             front++;
-    }
-
-    void pop(){
-        if((front==-1) || front>back){
-           cout << "No elements in queue" << endl;
-           return; 
         }
-        front++;
-    }
 
-    int  peek(){
-        if(front==-1 || front>back){
-           cout << "No elements in queue" << endl;
-           return -1; 
-        }
-        return arr[front];
-    }
+        int peek(){
+            if(front==-1 || front>back){
+                cout << "No elements in queue" << endl;
+                return -1; 
+            }
 
-    bool empty(){
-        if(front==-1 || front>back){
-           return true;
+            return arr[front];
         }
-        return false;
-    }
+
+        bool empty(){
+            if(front==-1 || front>back)
+                return true;
+            
+            return false;
+        }
 };
+
 
 int main(){
     queue q;
@@ -57,6 +61,7 @@ int main(){
     q.push(2);
     q.push(3);
     q.push(4);
+
     cout << q.peek() << endl;
     q.pop();
     cout << q.peek() << endl;
